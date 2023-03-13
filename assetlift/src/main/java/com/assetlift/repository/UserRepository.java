5e0c2350e93dd0f1f78834d89f5e698c516c9cc8
Luis Felipe <71433531+Talvez-ship-it@users.noreply.github.com>
2023-03-13 16:32:34 -0300

    Delete assetlift directory

--- assetlift/src/main/java/com/assetlift/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.assetlift.repository;
-
-import com.assetlift.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
