50564842dd5e9b4a4fd6896b777a9ca4b1830a6e
Luis Felipe Ramos da Silva <luisrs0412@ibm.com>
2023-03-13 23:29:29 -0300

    changing cors policy

--- src/main/java/com/assetlift/service/CorsConfiguration.java
+++ src/main/java/com/assetlift/service/CorsConfiguration.java
@@ -12,7 +12,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5500")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .exposedHeaders("Content-Type")
